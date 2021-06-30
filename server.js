25cf07c41e96a1d9cf3417c751bbae8640753b8a
UPye <Urika.Pye@gmail.com>
2021-06-30 18:36:50 -0400

    Updated file structure and completed server.js file.

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lewis_socialNetwork_API', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/thesocialnetwork', {
   useFindAndModify: false,
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true
 });
 
